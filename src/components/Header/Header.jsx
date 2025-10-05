ad1c421c651c35946c30d0c4386f8f2872782246
Daniela <danielaguzman50@outlook.es>
2025-10-05 18:05:06 -0500

    se agrego responsive-menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,33 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Header.scss";
 import picture from "../../assets/dani.png";
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <header className="header">
       <div className="container-picture-name">
         <img src={picture} alt="Daniela Guzman" className="picture" />
         <h1 className="username">Daniela Guzman Avila</h1>
+
+        <span
+          className="material-symbols-outlined menu-icon"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? "close" : "menu"}
+        </span>
       </div>
-      <nav className="nav">
-        <Link to="../About/About.jsx"> About me </Link>
-        <Link to="../Projectss/Projects.jsx">Projects</Link>
-        <Link to="../Contact/Contact.jsx">Contacts</Link>
+
+      <nav className={`nav ${menuOpen ? "show" : ""}`}>
+        <Link to="../About/About.jsx" onClick={() => setMenuOpen(false)}>
+          About me
+        </Link>
+        <Link to="../Projectss/Projects.jsx" onClick={() => setMenuOpen(false)}>
+          Projects
+        </Link>
+        <Link to="../Contact/Contact.jsx" onClick={() => setMenuOpen(false)}>
+          Contact
+        </Link>
       </nav>
     </header>
   );
